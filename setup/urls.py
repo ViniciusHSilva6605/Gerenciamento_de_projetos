f0ae97b2e48b66491e0032be118f2628dd70a652
vinicciuus@gmail.com <108134808+ViniciusHSilva6605@users.noreply.github.com>
2023-10-15 23:38:21 -0300

    Update urls.py

--- setup/urls.py
+++ setup/urls.py
@@ -30,13 +30,14 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),  # Inclui as URLs de autenticação embutidas
     path("", TodoListView.as_view(), name="todo_list"),
+     path("companies/", CompanyListView.as_view(), name="company_list"),
     path("create", TodoCreateView.as_view(), name="todo_create"),
     path("update/<int:pk>", TodoUpdateView.as_view(), name="todo_update"),
     path("delete/<int:pk>", TodoDeleteView.as_view(), name="todo_delete"),
     path("complete/<int:pk>", TodoCompleteView.as_view(), name="todo_complete"),
     
     # Empresas
-    path("companies/", CompanyListView.as_view(), name="company_list"),
+    #path("companies/", CompanyListView.as_view(), name="company_list"),
     path("companies/create", CompanyCreateView.as_view(), name="company_create"),
     path("companies/update/<int:pk>", CompanyUpdateView.as_view(), name="company_update"),
     path("companies/delete/<int:pk>", CompanyDeleteView.as_view(), name="company_delete"),
