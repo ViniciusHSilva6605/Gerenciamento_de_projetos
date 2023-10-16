03ba9e40caa772915daa48482089f376eba94813
vinicciuus@gmail.com <108134808+ViniciusHSilva6605@users.noreply.github.com>
2023-10-16 04:15:57 -0300

    Update urls.py

--- setup/urls.py
+++ setup/urls.py
@@ -1,7 +1,4 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.contrib.auth import views as auth_views
-from django.urls import path, include
+from django.urls import path
 
 
 from todos.views import (
@@ -14,9 +11,6 @@ from todos.views import (
     ProjectUpdateView,
     ProjectDeleteView,
 )
-
-
-
 
 urlpatterns = [
    
